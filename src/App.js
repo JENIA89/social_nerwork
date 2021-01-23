658ed359e00b2a236e3d03659d685c4ea7d811e2
Evgene Popolamov <popolamov908@gmail.com>
2021-01-23 19:58:24 +0300

    feat: add user components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
+import User from './components/User/User';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <div className='app-wrapper-content'>
         <Route path='/dialogs' render={() => <DialogsContainer />} />
         <Route path='/profile' render={() => <Profile />} />
+        <Route path='/user' render={() => <User />} />
       </div>
     </div>
   );
---
+++ src/components/User/User.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const User = () => <div>User is here</div>;
+
+export default User;
